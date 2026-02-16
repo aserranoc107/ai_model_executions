d913edc6951ae2f650261b4de11d649a327e209a
fer.serranoc18@gmail.com <braincoding1802@gmail.com>
2026-02-16 13:58:53 -0600

    commiting changes

--- db.py
+++ db.py
@@ -1,27 +1,23 @@
+import os
+from dotenv import load_dotenv
 import boto3
 import json
 from sqlalchemy import create_engine
 
-DB_HOST = "localhost"
-DB_PORT = 5435
-DB_NAME = "demo"
-SECRET_NAME = "env/microservice/postgres-demo"
-REGION = "us-east-1"
-
-def get_secret():
-    client = boto3.client("secretsmanager", region_name=REGION)
-    response = client.get_secret_value(SecretId=SECRET_NAME)
-    secret = json.loads(response["SecretString"])
-    return secret
+load_dotenv()
 
 def get_engine():
-    secret = get_secret()
+    user = os.getenv("DB_USER")
+    password = os.getenv("DB_PASSWORD")
+    host = os.getenv("DB_HOST")
+    port = os.getenv("DB_PORT")
+    db = os.getenv("DB_NAME")
 
     connection_string = (
-        f"postgresql+psycopg2://{secret['username']}:"
-        f"{secret['password']}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+        f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db}"
     )
+    
+    return create_engine(connection_string, echo=False)
 
-    engine = create_engine(connection_string, echo=False)
-    return engine
+
 
